ccc14eba4ef97efd4747292fe22d82bcb2c6d73f
hyunwookoo13 <kyo972@naver.com>
2022-01-27 13:37:41 +0900

    solved baekjoon

---
+++ bj_2012.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = []
+
+for _ in range(n):
+    arr.append(int(input()))
+
+arr.sort()
+
+result = 0
+for i in range(1,len(arr)+1):
+    result += abs(i-arr[i-1])
+
+print(result)
